c9e829585740537b8af359d7b648981a5665678f
mintesnot <mintesnotyess@gmail.com>
2025-02-16 23:51:10 +0300

    edit form section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,30 @@ document.getElementById('menu-btn').addEventListener('click', function () {
 
     }
 });
+const currentYear = new Date().getFullYear();
+document.getElementById('year').innerHTML = currentYear;
+
+
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('message');
+
+
+    if (email.value === '') {
+        email.focus();
+    } if (subject.value === '') {
+        subject.focus();
+    } if (message.value === '') {
+        message.focus();
+    } if (email.value && subject && message) {
+        var mailto_link = 'mailto:Info@ses-groups.com';
+        mailto_link += '?subject=' + encodeURIComponent(subject.value);
+        mailto_link += '&body=' + encodeURIComponent(message.value);
+        window.open(mailto_link, '_blank');
+    }
+
+
+})
